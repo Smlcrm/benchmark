59ccf7502ef0709f8e8cbe0d6e07f4c117c4a518
Alif Abdullah <alifabdullah7040@gmail.com>
2025-06-16 19:47:18 -0400

    Pushing what I have for the hyperparameter trainer so far

---
+++ benchmarking_pipeline/trainer/hyperparameter_tuning.py
@@ -0,0 +1,22 @@
+import itertools
+
+class HyperparameterTuner:
+  def __init__(self, model_class, *hyperparameter_ranges):
+    self.model_class = model_class
+    self.hyperparameter_ranges = hyperparameter_ranges
+  
+  def hyperparameter_grid_search_single_time_series(self, time_series):
+    best_trained_model = None
+    best_hyperparameters = None
+    lowest_train_loss = float("inf")
+    for hyperparameter_setting in itertools.product(*self.hyperparameter_ranges):
+      trained_model = self.model_class.train(time_series)
+      current_train_loss = self.model_class.evaluate(time_series)
+
+      if current_train_loss < lowest_train_loss:
+        best_trained_model = trained_model
+        best_hyperparameters = hyperparameter_setting
+    
+    return best_trained_model, best_hyperparameters
+  
+  
