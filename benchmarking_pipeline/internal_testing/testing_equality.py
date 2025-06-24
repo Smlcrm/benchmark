438e30c091282e194a82768404fc0df5f3e029ac
Alif Abdullah <alifabdullah7040@gmail.com>
2025-06-24 06:52:03 -0400

    Modified testing equality to work with preprocessor

--- benchmarking_pipeline/internal_testing/testing_equality.py
+++ benchmarking_pipeline/internal_testing/testing_equality.py
@@ -4,7 +4,7 @@ import numpy as np
 def value_equality(value_0, value_1):
   assert type(value_0) == type(value_1), f"Type '{type(value_0)}' for the first value does not match Type '{type(value_1)}' of the second value."
   if isinstance(value_0, pd.Series):
-    return (value_0 == value_1).all()
+    return np.allclose(value_0, value_1)
   else:
     return value_0 == value_1
   
